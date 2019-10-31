20b7af85dcd01370513543acde4c1a79ac27c8e3
Cherrera543 <cherrera543@ksu.edu>
2019-10-30 23:20:17 -0500

    Created main menu for method options

---
+++ ChocolateMethods.cs
@@ -0,0 +1,69 @@
+using System;
+public class ChocolateMethods{
+	public static void Main(string[] args){
+		char redo = 'y';
+		while (redo=='y'){
+			int option;
+			Console.WriteLine("Possible Methods:");
+			Console.WriteLine("Method 1: Reverse the word order in a phrase.");
+			Console.WriteLine("Method 2: Find the second largest integer in an array.");
+			Console.WriteLine("Method 3: Find if an integer is a prime number.");
+			Console.WriteLine("What method would you like to try? (Ex. 1, 2, 3)");
+			string input = Console.ReadLine();
+			while(!input.Equals("1") && !input.Equals("2") && !input.Equals("3")){
+				Console.WriteLine("Not a method option. Please enter 1, 2, or 3");
+				input = Console.ReadLine();
+				Console.WriteLine("-"+ input +"-");
+			}
+			option = Convert.ToInt32(input);
+			switch(option){
+				case 1:
+					Console.WriteLine("What phrase would you like to reverse?");
+					string phrase = Console.ReadLine();
+					ReverseWordOrder(phrase);
+					break;
+				case 2:
+					Console.WriteLine("Please enter your list of numbers seperated by spaces:");		
+					int[] array = CreateIntArr(Console.ReadLine());
+					FindSecondLargeInArray(array);
+					break;
+				case 3:
+					Console.WriteLine("Please enter the number you would like to check:");
+					int number = CheckifNumber(Console.ReadLine());
+					if(FindPrime(number)) Console.WriteLine("Prime");
+					else Console.WriteLine("Not Prime");
+					break;
+			}
+			Console.WriteLine("Would you like to try another method? Y or N");
+			string answer = Console.ReadLine();
+			redo = char.ToLower(answer[0]);
+			while(redo != 'y' && redo != 'n'){ 
+				Console.WriteLine("Please reenter: ");
+				answer = Console.ReadLine();
+				redo = char.ToLower(answer[0]);
+			}
+				
+		}
+	}
+		
+	internal static void ReverseWordOrder(string str){
+			
+	}
+	private static int[] CreateIntArr(string str){
+		int[] arr = new int[0];
+		return arr;
+	}
+	internal static void FindSecondLargeInArray(int[] arr){
+			
+	}
+	private static int CheckifNumber(string str){
+		int num = 0;
+		return num;
+	}
+	internal static bool FindPrime(int number){
+		bool prime = true;
+		return prime;
+	}
+		
+	
+}
